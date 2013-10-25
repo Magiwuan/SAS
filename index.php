3b56cdc4468dac7c7e25ed842d9316ee6ae2fbf2
Andrez <andrez.alvarado@gmail.com>
2013-10-25 06:15:38 -0430

    update
    css

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="es">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>.:Acceso:.</title>
-  	<link rel="stylesheet" type="text/css" href="persona/Css/estilo2.css" />       
-   	<link rel="stylesheet" type="text/css" href="persona/Css/estilo.css" /> 
+  	 
     <link href="persona/JavaScript/jquery.alerts.css" rel="stylesheet" type="text/css" />	 
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
    <script language="javascript" type="text/javascript" src="persona/JavaScript/jquery-1.4.2.min.js"></script>     
    <script language="javascript" type="text/javascript" src="persona/JavaScript/jquery.alerts.js"></script> 
    <script language="JavaScript" type="text/javascript" src="persona/JavaScript/jquery.asmselect.js"></script>	
@@ -28,10 +28,10 @@
 #capa3{
 	position: relative;	
 	width: 305px;
-	height: 325px;
+	height: 150px;
 	margin: auto;
+	margin-top:40px;
 	background: #fff;
-	padding-top: 100px;
 	}
 	.fiefieldset{
 		border-radius: 5px;
@@ -40,25 +40,27 @@
 </head>
 <body>
 <div id="capa3">
-  <fieldset class="fiefieldset">
+
     <table width="209" height="110" cellpadding="4" cellspacing="1" align="center">
       <form action="persona/Controladores/control_acceso.php" method="post" name="registro" id="registro" onsubmit="if(!Enviar()) {return false;};">
         <tr valign="middle" align="left">
-          <td style="font-size: 14px; vertical-align: middle; color: #500; font-family: Georgia, 'Times New Roman', Times, serif;" width="64" align="right" height="25">Usuario:</td>
-          <td width="111" height="25" align="center"><input name="login" align="center" style="border: 1px solid; color:#006; width: 95px; font-family: tahoma; font-size: 11px;" type="text" /></td>
+          <td width="64" align="right" height="25">Usuario:</td>
+          <td width="111" height="25" align="center"><input name="login" align="center"type="text" /></td>
+          <td width="111" align="center">&nbsp;</td>
         </tr>
         <tr valign="middle" align="left">
-          <td style="font-size: 14px; vertical-align: middle; color: #500; font-family: Georgia, 'Times New Roman', Times, serif;" width="64" align="right" height="25">Clave :</td>
-          <td height="25" align="center"><input name="clave" align="middle" style="border: 1px solid; color:#006;; width: 95px; font-family: tahoma; font-size: 11px; " type="password" /></td>
+          <td width="64" align="right" height="25">Clave :</td>
+          <td height="25" align="center"><input name="clave" align="middle" type="password" /></td>
+          <td align="center">&nbsp;</td>
         </tr>
         <tr valign="middle" align="left">
           <td width="64" align="right" height="25">&nbsp;</td>
-          <td style="vertical-align: middle;" align="left" height="25"><input name="boton1" value=" Entrar " type="submit" id="boton1"/>
+          <td style="vertical-align: middle;" align="left" height="25"><input name="boton1" class="btn-primary " value=" Entrar " type="submit" id="boton1"/>
             <input name="operacion" type="hidden" value="1" /></td>
+          <td style="vertical-align: middle;" align="left">&nbsp;</td>
         </tr>
       </form>
     </table>
-    </fieldset>
-</div>
+    </div>
 </body>
 </html>
